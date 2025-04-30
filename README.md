d53fe2c523505414057b0c7768296e9a5688db7d
inacio88 <inacio88@outlook.com>
2025-04-29 21:58:25 -0300

    main: configuracao banco

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Banco:
 docker run --name api-cliente-postgres -e POSTGRES_PASSWORD=1q2w3e4r@@@ -d -p 5436:5432 postgres
+
+Stringconnection pelo user secrets ( mas também pode ser variável de ambiente normal)
+dotnet user-secrets set conexao "User ID=postgres;Password=1q2w3e4r@@@;Host=localhost;Port=5436;Database=ClienteDB;Pooling=true;"
--- api.cliente/Program.cs
+++ api.cliente/Program.cs
@@ -1,18 +1,23 @@
+using api.cliente.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+string conexao = builder.Configuration.GetValue<string>("conexao") ?? String.Empty;
+builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.UseNpgsql(conexao);
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
